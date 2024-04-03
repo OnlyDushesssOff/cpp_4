aaf199c3342883fff0b1263fc941ff0d856c817d
OnlyDushesssOff <andreyrabotaem825@gmail.com>
2024-04-03 23:49:09 +0300

    123

--- Vector.h
+++ Vector.h
@@ -1,51 +1,143 @@
 #include <iostream>
+#include <math.h>
+#include <cstdint>
 
 template<typename T>
-class Vector{
+class Vector {
 private:
     T* _array;
     size_t _size;
+    double _leng;
+
+    double Leng() {
+        double sum = 0;
+        for (int i = 0; i < _size; i++) {
+            sum += pow(_array[i], 2);
+        }
+        return pow(sum, 0.5);
+    }
 
 public:
     Vector() = delete; // если дефолтный конструктор не нужен, то пишем так, просто не написать его не получится
-    Vector(size_t size, T* array):_size(size), _array(new T[size]){// если используются шаблоны, то лучше писать это в хидере, в срр это будет долго и нудно
-        for(int i = 0; i < _size; i++){
+    Vector(size_t size, T* array) :_size(size), _array(new T[size]) {// если используются шаблоны, то лучше писать это в хидере, в срр это будет долго и нудно
+        for (int i = 0; i < _size; i++) {
             _array[i] = array[i];
         }
+        _leng = Leng();
     }
 
-    Vector(size_t size): _size(size) {// параметры можно выносить через двоеточие, но хз зачем оно надо
+    Vector(size_t size) : _size(size) {// параметры можно выносить через двоеточие, но хз зачем оно надо
         _array = new T[size];
-        for(int i = 0; i < _size; i++)
+        for (int i = 0; i < _size; i++)
             _array[i] = 0;
+        _leng = Leng();
     }
 
-    Vector(std::initializer_list<T> list){ // конструктор для того, чтобы нароямую задавать векторы
+    Vector(std::initializer_list<T> list) { // конструктор для того, чтобы нароямую задавать векторы
         _size = list.size();
         _array = new T[_size];
         int i = 0;
-        for(auto elem = list.begin(); elem != list.end(); elem++, i++){
+        for (auto elem = list.begin(); elem != list.end(); elem++, i++) {
             _array[i] = *elem;
+        _leng = Leng();
         }
     }
 
-    ~Vector(){ // деструктор, самоудаляется, когда перестает испоользоваться
-        delete [] _array;
+    ~Vector() { // деструктор, самоудаляется, когда перестает испоользоваться
+        delete[] _array;
     }
 
-    T& operator[](int i){
+    T& operator[](int i) {
         return _array[i];
     }
 
-    friend std::ostream& operator<< (std::ostream& os, const Vector& vec){
+    inline int GetSize() const {
+        return _size;
+    }
+
+    inline T* GetArray() const {
+        return _array;
+    }
+
+    inline int GetLeng() const {
+        return _leng;
+    }
+
+    Vector<double> Normaliz(){
+        double* array = new double[_size];
+        for(int i = 0; i < _size; i++){
+            array[i] = _array[i] / _leng;
+        }
+        Vector<double> res = Vector<double>(_size, array);
+        return res;
+    }
+
+    friend std::ostream& operator<< (std::ostream& os, const Vector& vec) {
         os << "(";
-        for(int i = 0; i < vec._size; i++){
+        for (int i = 0; i < vec._size; i++) {
             os << vec._array[i];
-            if(i+1 != vec._size)
+            if (i + 1 != vec._size)
                 os << ", ";
         }
         os << ")";
         return os;
+    }
+
+    template<typename F>
+    Vector operator+ (const Vector<F>& vec) const {
+        size_t size = vec.GetSize();
+        auto* array = vec.GetArray();
+        if (_size != size) {
+            std::cout << "Error input";
+            exit(111);
+        }
+        T* array_res = new T[_size];
+        for (int i = 0; i < _size; i++) {
+            array_res[i] = _array[i] + array[i];
+        }
+        Vector vector = Vector(size, array_res);
+        return vector;
+    }
+
+    template<typename F>
+    Vector operator- (const Vector<F>& vec) const {
+        size_t size = vec.GetSize();
+        auto* array = vec.GetArray();
+        if (_size != size) {
+            std::cout << "Error input";
+            exit(111);
+        }
+        T* array_res = new T[_size];
+        for (int i = 0; i < _size; i++) {
+            array_res[i] = _array[i] - array[i];
+        }
+        Vector vector = Vector(size, array_res);
+        return vector;
+    }
+    
+    template<typename F>
+    double operator* (const Vector<F>& vec) const {
+        size_t size = vec.GetSize();
+        auto* array = vec.GetArray();
+        if (_size != size) {
+            std::cout << "Error input";
+            exit(111);
+        }
+        double scal = 0;
+        for(int i = 0; i < _size; i++)
+            scal += _array[i] * array[i];
+        return scal;
+    }
+
+    Vector operator=(const Vector& vec){
+        _size = vec.GetSize();
+        _leng = vec.GetLeng();
+        _array = vec.GetArray();
+        return *this;
+    }
+
+    inline void SetArray( T* array){
+        _array = array;
     }
 };
 /* Домашка:
--- main.cpp
+++ main.cpp
@@ -1,17 +1,9 @@
 #include "Vector.h"
 
 int main(){
-    double* array_for_vec2 = new double[10]{
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-    };
-    double arr[] = {1, 3, 4, 6, 8};
-    Vector<int> vec3 = {1,3,4,6,8};
-    Vector<int> vec1 = Vector<int>(10);
-    Vector<double> vec2 = Vector<double>(10, array_for_vec2);
 
-    std::cout << vec1 << std::endl;
-    std::cout << vec2 << std::endl;
-    std::cout << vec3 << std::endl;
-
-    delete [] array_for_vec2;
+    Vector<int> vector2 = {6, 8};
+    Vector<int> vector3 = { 0, 0 };
+    vector3 = vector2; 
+    std::cout << vector3;  
 }
